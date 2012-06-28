6e29df753fd4781d3dce82c8f1f3d2afe2d4095c
Joseph Wecker <joseph.wecker@gmail.com>
2012-06-27 21:35:32 -0700

    Output some debug info & narrow the scope of the pool

--- test/test_gx_mfd.c
+++ test/test_gx_mfd.c
@@ -3,13 +3,13 @@
 
 gx_error_initialize(GX_DEBUG);
 
-static gx_mfd_pool  *mfd_pool;
-
 int main(int argc, char **argv) {
+    gx_mfd_pool  *mfd_pool;
     gx_mfd       *mfd;
     Xn( mfd_pool = new_gx_mfd_pool(1)                  ){X_ERROR; X_EXIT;}
     Xn( mfd = acquire_gx_mfd(mfd_pool)                 ){X_ERROR; X_EXIT;}
     X(  gx_mfd_open_writer(mfd, "tmp/writer-file.dat") ){X_ERROR; X_EXIT;}
 
-
+    printf("%llx\n", mfd->head->sig);
+    return 0;
 }
