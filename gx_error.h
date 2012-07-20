f8e265c5e53ba46d2337df7257eaf866497d3444
jeff <jeff@justin.tv>
2012-07-19 20:00:36 -0700

    flexible logging

--- gx_error.h
+++ gx_error.h
@@ -95,6 +95,7 @@
 #include <stdarg.h>
 #include <unistd.h> /* TODO: pipe logger for atomic msgs to stdout */
 
+
 /// Place this somewhere in global scope
 #define gx_error_initialize(default_loglevel)               \
     gx_error_report gx_error_recent_reports[3];             \
@@ -106,68 +107,68 @@
 
 /// For system errors- detects -1 and uses errno
 #define cX(expr, ctx) if( ({errno=0; (gx_unlikely((int)(expr) == -1)) ?        \
-    ({_GX_X(ctx,errno,0,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(ctx,errno,0,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// Checks for null values
 #define cXn(expr, ctx) if( ({errno=0; (gx_unlikely((void *)(expr) == NULL)) ?  \
-    ({_GX_X(ctx,errno,1,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(ctx,errno,1,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// Check for mmap errors
 #define cXm(expr, ctx) if( ({errno=0; (gx_unlikely((expr) == MAP_FAILED)) ?    \
-    ({_GX_X(ctx,errno,2,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(ctx,errno,2,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// Checks for system errors, but drops you into a switch statement
 #define cXs(expr, ctx) if( ({errno=0; (gx_unlikely((int)(expr) == -1)) ?       \
-    ({_GX_X(ctx,errno,0,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;})) \
+    ({_GX_X(ctx,errno,0,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;})) \
     switch(gx_error_recent_reports[gx_error_curr_report].sys_errno)
 
 /// For system errors- detects -1 and uses errno
 #define X(expr) if( ({errno=0; (gx_unlikely((int)(expr) == -1)) ?        \
-    ({_GX_X(NULL,errno,0,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(NULL,errno,0,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// Checks for null values
 #define Xn(expr) if( ({errno=0; (gx_unlikely((void *)(expr) == NULL)) ?  \
-    ({_GX_X(NULL,errno,1,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(NULL,errno,1,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// Check for mmap errors
 #define Xm(expr) if( ({errno=0; (gx_unlikely((expr) == MAP_FAILED)) ?    \
-    ({_GX_X(NULL,errno,2,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({_GX_X(NULL,errno,2,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 /// For functions that return zero on success, and errno on error
 #define Xz(expr) if( ({int _gx_tmp_errno=0; errno=0; (gx_unlikely((_gx_tmp_errno = (int)(expr)) != 0)) ? \
-    ({errno=_gx_tmp_errno; _GX_X(NULL,_gx_tmp_errno,2,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;}))
+    ({errno=_gx_tmp_errno; _GX_X(NULL,_gx_tmp_errno,2,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;}))
 
 
 /// Checks for system errors, but drops you into a switch statement
 #define Xs(expr) if( ({errno=0; (gx_unlikely((int)(expr) == -1)) ?       \
-    ({_GX_X(NULL,errno,0,NULL,__FILE__,__LINE__,__FUNCTION__,#expr);1;}):0;})) \
+    ({_GX_X(NULL,errno,0,NULL,__FILE__,__STR(__LINE__),__FUNCTION__,#expr);1;}):0;})) \
     switch(gx_error_recent_reports[gx_error_curr_report].sys_errno)
 
 #define X_LOG_PANIC(msg, ...) \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_PANIC;1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_PANIC;1;})
 #define X_LOG_FATAL(msg, ...) \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_FATAL;1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_FATAL;1;})
 #define X_LOG_ERROR(msg, ...) \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_ERROR;1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_ERROR;1;})
 #define X_LOG_WARN( msg, ...)  \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_WARN; 1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_WARN; 1;})
 #define X_LOG_INFO( msg, ...)  \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_INFO; 1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_INFO; 1;})
 #define X_LOG_DEBUG(msg, ...) \
-    ({_GX_X(NULL,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_DEBUG;1;})
+    ({_GX_X(NULL,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_DEBUG;1;})
 
 #define cX_LOG_PANIC(ctx, msg, ...) \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_PANIC;1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_PANIC;1;})
 #define cX_LOG_FATAL(ctx, msg, ...) \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_FATAL;1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_FATAL;1;})
 #define cX_LOG_ERROR(ctx, msg, ...) \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_ERROR;1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_ERROR;1;})
 #define cX_LOG_WARN(ctx, msg, ...)  \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_WARN; 1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_WARN; 1;})
 #define cX_LOG_INFO(ctx, msg, ...)  \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_INFO; 1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_INFO; 1;})
 #define cX_LOG_DEBUG(ctx, msg, ...) \
-    ({_GX_X(ctx,0,3,msg,__FILE__,__LINE__,__FUNCTION__,NULL,##__VA_ARGS__);X_DEBUG;1;})
+    ({_GX_X(ctx,0,3,msg,__FILE__,__STR(__LINE__),__FUNCTION__,NULL,##__VA_ARGS__);X_DEBUG;1;})
 
 #define X_PANIC _GX_ERROR_LOG(GX_PANIC) ///< Log last captured error at 'panic' severity
 #define X_FATAL _GX_ERROR_LOG(GX_FATAL) ///< Log last captured error at 'fatal' severity
@@ -198,7 +199,7 @@ typedef struct gx_error_report {
     int   gx_errno;
     char  custom_msg[1024];
     char  filename[256];
-    int   linenum;
+    const char* linenum;
     char  function[256];
     char  expression[256];
 } gx_error_report;
@@ -277,44 +278,57 @@ extern int             gx_error_loglevel;
 #endif
 
 /* Some predefined color escape sequences */
-#ifndef LOG_PLAIN
-    #define C_N               "\e[0m"
-    #define C_PANIC           "\e[38;5;198m"
-    #define C_FATAL           "\e[38;5;196m"
-    #define C_ERROR           "\e[38;5;160m"
-    #define C_WARN            "\e[38;5;202m"
-    #define C_INFO            "\e[38;5;118m"
-    #define C_DEBUG           "\e[38;5;226m"
-    #define C_UNKN            "\e[38;5;27m"
-    #define C_DELIM           "\e[38;5;236m"
-    #define C_BG              "\e[48;5;234m"
-    #define C_REF             "\e[38;5;240m"
-#else 
-    #define C_N               ""
-    #define C_PANIC           ""
-    #define C_FATAL           ""
-    #define C_ERROR           ""
-    #define C_WARN            ""
-    #define C_INFO            ""
-    #define C_DEBUG           ""
-    #define C_UNKN            ""
-    #define C_DELIM           ""
-    #define C_BG              ""
-    #define C_REF             ""
-#endif
+#define C_N               "\e[0m"
+#define C_PANIC           "\e[38;5;198m"
+#define C_FATAL           "\e[38;5;196m"
+#define C_ERROR           "\e[38;5;160m"
+#define C_WARN            "\e[38;5;202m"
+#define C_INFO            "\e[38;5;118m"
+#define C_DEBUG           "\e[38;5;226m"
+#define C_UNKN            "\e[38;5;27m"
+#define C_DELIM           "\e[38;5;236m"
+#define C_BG              "\e[48;5;234m"
+#define C_REF             "\e[38;5;240m"
 
 #define _START   C_BG C_DELIM "["
 #define _DLM(ch) C_BG C_DELIM ch C_REF   // delimiter shorthand for internal use
 #define _FINISH  C_DELIM "]" C_N " "
 
+//wrapper around stringifying a macro arg to avoid affecting the function
+#define __STR2(x) #x
+#define __STR(x) __STR2(x)
+
+//a trick to get the number of args passed to a variadic macro
+#define PP_NARG(...) \
+         PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
+#define PP_NARG_(...) \
+         PP_ARG_N(__VA_ARGS__)
+#define PP_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,N,...) N
+#define PP_RSEQ_N() \
+         63,62,61,60,                   \
+         59,58,57,56,55,54,53,52,51,50, \
+         49,48,47,46,45,44,43,42,41,40, \
+         39,38,37,36,35,34,33,32,31,30, \
+         29,28,27,26,25,24,23,22,21,20, \
+         19,18,17,16,15,14,13,12,11,10, \
+         9,8,7,6,5,4,3,2,1,0
+
 static char *_gx_eloglvl[] = {
-    C_PANIC "panic",
-    C_FATAL "fatal",
-    C_ERROR "error",
-    C_WARN  "warn ",
-    C_INFO  "info ",
-    C_DEBUG "debug"
+    "panic",
+    "fatal",
+    "error",
+    "warn",
+    "info",
+    "debug"
 };
+
 static char *_gx_errmsgs[] = {
     "System error.",
     "NULL where value is expected.",
@@ -325,13 +339,14 @@ static char *_gx_errmsgs[] = {
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 5)
 #pragma GCC diagnostic ignored "-Wunused-function"
 #endif
+
 static void _GX_X(
         void       *context,
         int         sys_errno,
         int         gx_errno,
         const char *custom_msg,
         const char *filename,
-        int         linenum,
+        const char *linenum,
         const char *function,
         const char *expression, ...) {
     va_list ap;
@@ -370,13 +385,127 @@ static GX_INLINE void _gx_get_expr_part(char *expr, char *exprpart) {
     }
 }
 
+
+#define LOG_PIPES 0
+#define LOG_JSON  1
+#define LOG_STDOUT 1
+#define LOG_STDERR 2
+#ifndef LOG_FORMAT 
+    #define LOG_FORMAT LOG_PIPES
+#endif
+
+typedef struct logger {
+    int handle;
+    uint8_t format;
+    const char *color;
+} logger;
+
+typedef struct log_format {
+    char *begin, *kvformat, *delimiter, *end;
+} log_format;
+
+typedef struct kv {
+    struct kv *_next, *_prev, *next;
+    const char *key, *value;
+} kv;
+
+typedef struct context {
+    struct context *_next, *_prev;
+    kv* keyvalues;
+} context;
+
+// >> log(ctx, level, msg("hello %s", "jeff"), "key", "value")
+// >> inner_log(ctx, level, 
+//      PP_NARG("key", "value") + 6, 
+//      msg("hello %s", "jeff"), "key", "value", "line", line, "function", function, "file", file)
+// >> inner_log(ctx, level, 
+//      PP_NARG("key", "value") + 6, 
+//      "hello %s", "jeff", "key", "value", "line", line, "function", function, "file", file)
+// >> inner_log(ctx, level, 
+//      8, 
+//      "hello %s", "jeff", "key", "value", "line", line, "function", function, "file", file)
+
+
+#define LOG(ctx, level, message, ...)                                        \
+    log_inner(ctx, level,                                                    \
+        PP_NARG(__VA_ARGS__) + 8,                                            \
+        message,                                                             \
+        __VA_ARGS__,                                                         \
+        "levl", _gx_eloglvl[level],                                          \
+        "file", __FILE__,                                                    \
+        "func", __FUNCTION__,                                                \
+        "line", __STR(__LINE__)                                              \
+    )                                                      
+
+#define MSG(str, ...) str, __VA_ARGS__
+
+
+static logger loggers[] = {
+    /*panic*/ {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_PANIC},
+    /*fatal*/ {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_FATAL},
+    /*error*/ {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_ERROR},
+    /*warn*/  {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_WARN},
+    /*info*/  {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_INFO},
+    /*debug*/ {.handle = LOG_STDERR, .format = LOG_FORMAT, .color = C_DEBUG}
+};
+
+static log_format formats[] = {
+    /* pipes */ {.begin = "",  .kvformat = "%s:%s",         .delimiter = "|",  .end = "" },
+    /* json  */ {.begin = "{", .kvformat = "\"%s\":\"%s\"", .delimiter = ", ", .end = "}"}
+};
+
+#define LINE_MAX 1024
+#define BUFFER_MAX 1024
+#define MESSAGE_MAX 512
+static GX_INLINE void log_inner(context *ctx, int level, size_t kv_argc, const char* printf_str, ...) {
+    unsigned int n;
+    char line[LINE_MAX] = "";
+    char buffer[BUFFER_MAX] = "";
+    char message[MESSAGE_MAX] = "";
+    logger* logger = &loggers[level];
+
+    va_list argp;
+    va_start(argp, printf_str);
+    //sort of sketchy--we expect here that vsnprintf calls va_arg(argp) the right number of times
+    //but works here and is the way the implementation works from what i've seen
+    vsnprintf(message, MESSAGE_MAX, printf_str, argp);
+    snprintf(buffer, BUFFER_MAX, "%s%s%s", logger->color, message, C_N);
+    strncpy(message, buffer, MESSAGE_MAX);
+    strncat(line, formats[logger->format].begin, LINE_MAX);
+    snprintf(buffer, BUFFER_MAX, formats[logger->format].kvformat, "mesg", message);
+    strncat(line, buffer, LINE_MAX - strlen(line));
+    for (n = 0; n < kv_argc; n += 2) {
+        const char* key   = va_arg(argp, const char*);
+        const char* value = va_arg(argp, const char*);
+        strncat(line, formats[logger->format].delimiter, LINE_MAX - strlen(line));
+        snprintf(buffer, BUFFER_MAX, formats[logger->format].kvformat, key, value);
+        strncat(line, buffer, LINE_MAX - strlen(line));
+    }
+    va_end(argp);
+
+    if (ctx != NULL) {
+        kv *keyvalue = ctx->keyvalues;
+        while (keyvalue != NULL) {
+            snprintf(buffer, BUFFER_MAX, formats[logger->format].kvformat, keyvalue->key, keyvalue->value);
+            strncat(line, buffer, LINE_MAX - strlen(line));
+            keyvalue = keyvalue->next;
+        }
+    }
+    strncat(line, formats[logger->format].end, LINE_MAX - strlen(line));
+
+    flock(logger->handle, LOCK_EX);
+    write(logger->handle, line, strlen(line));
+    write(logger->handle, "\n", 1);
+    flock(logger->handle, LOCK_UN);
+}
+
 static void _GX_ERROR_LOG(int severity) {
     if(gx_unlikely(gx_error_loglevel >= severity)) {
         gx_error_report *rp = &gx_error_recent_reports[gx_error_curr_report];
         char basename[20];
         char expr_part[20];
         char sys_errormsg[1024];
-        char final_logline[4096];
+        //char final_logline[4096];
         char *fname;
         _gx_get_basename(rp->filename, basename);
         _gx_get_expr_part(rp->expression, expr_part);
@@ -404,25 +533,20 @@ static void _GX_ERROR_LOG(int severity) {
             fname[0] = rp->function[0];
         }
 
-        // [SEVERITY:ENAME:basename:linenum:function/exprpart] sysmsg | custom_msg
-        (void) snprintf(final_logline, 4095,
-            _START "%s"    /* severity     */ _DLM(":")
-                   "%s"    /* sys-errname  */ _DLM(":")
-                   "%-17s" /* src basename */ _DLM(":")
-                   "%4d"   /* line number  */ _DLM(":")
-                   "%-17s" /* fun/err-fun  */
-            _FINISH
-                "%s\n", /* system error-message & user error-message */
-            _gx_eloglvl[severity],
-            _gx_ename[rp->sys_errno],
-            basename,
-            rp->linenum,
-            fname,
-            sys_errormsg);
-        flockfile(stderr);
-        fprintf(stderr, "%s", final_logline);
-        funlockfile(stderr);
+        if (rp->sys_errno != 0) {
+            log_inner(NULL, severity, 10, sys_errormsg, 
+                "levl", _gx_eloglvl[severity],
+                "name", _gx_ename[rp->sys_errno], 
+                "file", basename, 
+                "line", rp->linenum,
+                "func", fname);
+        } else {
+            log_inner(NULL, severity, 8, sys_errormsg, 
+                "levl", _gx_eloglvl[severity],
+                "file", basename, 
+                "line", rp->linenum,
+                "func", fname);
+        }
     }
 }
-
 #endif
