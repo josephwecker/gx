2d0640837355b3937e348b5eeee5eee3409d0940
Joseph Wecker <joseph.wecker@gmail.com>
2012-07-20 12:42:44 -0700

    starting to get a unique "node" identifier built

--- gx_net.h
+++ gx_net.h
@@ -6,10 +6,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <ifaddrs.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <gx/gx.h>
+
 
 static GX_INLINE int gx_net_tcp_open(const char *host, const char *port) {
     int             ires;
@@ -86,4 +88,62 @@ static GX_INLINE int gx_net_daemonize(void) {
 
     return 0;
 }
+
+
+#if 0
+static char _gx_node_agg_id_memoized[12] = {'\0'};
+
+static GX_INLINE int gx_node_agg_id(char *buf, size_t len) {
+    if(gx_likely(_gx_node_agg_id_memoized[0])) {
+        memcpy
+    }
+
+
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Walk through linked list, maintaining head pointer so we
+       can free list later */
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+
+        /* Display interface name and family (including symbolic
+           form of the latter for the common families) */
+
+        printf("%s  address family: %d%s\n",
+                ifa->ifa_name, family,
+                //(family == AF_PACKET) ? " (AF_PACKET)" :
+                (family == AF_INET) ?   " (AF_INET)" :
+                (family == AF_INET6) ?  " (AF_INET6)" : "");
+
+        /* For an AF_INET* interface address, display the address */
+
+        if (family == AF_INET || family == AF_INET6) {
+            s = getnameinfo(ifa->ifa_addr,
+                    (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                    sizeof(struct sockaddr_in6),
+                    host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            printf("\taddress: <%s>\n", host);
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return 0;
+}
+#endif
+
 #endif
