688302b7c081d743a76c0df9cf185d5d3917b53e
Joseph Wecker <jwecker@justin.tv>
2012-09-05 20:16:18 -0700

    Going to try to completely clean up the error/logging capabilities since I use them the most extensively

---
+++ gx_error2.h
@@ -0,0 +1,30 @@
+/**
+ * @file      gx_error.h
+ * @brief     Lots of error-handling/logging to help unobscure C code.
+ *
+ * @author    Joseph A Wecker <joseph.wecker@gmail.com>
+ * @copyright
+ *   Except where otherwise noted, Copyright (C) 2012 Joseph A Wecker
+ *
+ *   MIT License
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ *   copy of this software and associated documentation files (the "Software"),
+ *   to deal in the Software without restriction, including without limitation
+ *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *   and/or sell copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ *
+ */
+
