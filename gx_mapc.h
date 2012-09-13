c41210dc4af0f6235fa52d8b86313a7861f76fae
Joseph Wecker <jwecker@justin.tv>
2012-09-13 13:35:17 -0700

    Changed mmipc to mapc so it's pronouncable

--- gx_mapc.h
+++ gx_mapc.h
@@ -5,7 +5,7 @@
  * Essentially for very fast essentially zero-copy bit-shoveling. A writer can
  * write to the structure either directly to memory or via standard I/O
  * functions and a reader in a potentially different process looking at the
- * same mmipc will be immediately notified about new content (via a fd that can
+ * same mapc will be immediately notified about new content (via a fd that can
  * be put in select/poll/epoll and friends) immediately- even before the data
  * syncs to disk and even if the data never goes to disk at all. The reader can
  * then use sendfile or other zero-copy mechanisms to very quickly copy the
@@ -17,8 +17,8 @@
  * them while still taking advantage of the speed of having all of the data
  * already in kernel-space.
  *
- * If an mmipc is marked as not being persistent and it is observed that the
- * path is on a tmpfs / ram-disk based filesystem (like /dev/shm) mmipc can go
+ * If an mapc is marked as not being persistent and it is observed that the
+ * path is on a tmpfs / ram-disk based filesystem (like /dev/shm) mapc can go
  * a step further and completely free pages as soon as the writer and all known
  * readers are done with them. This effectively frees up the RAM or swap space
  * for all data that has been completely communicated/transfered.
@@ -52,43 +52,42 @@
 #include <sys/stat.h>
 
 
-
-//------------------------------------------------------------------------------
 /// Mapped to the first part of the first page of the file- common offsets for mfds.
-typdef struct gx_mmipc_head {
+
+typdef struct gx_mapc_head {
     uint64_t file_sig;         ///< Will always be 0x1c... (so we don't clobber some unsuspecting file)
     uint64_t file_size;        ///< Full size of a file- including head(?)
     uint64_t start_available;  ///< File offset to data that hasn't been freed
     uint64_t start_active;     ///< File offset to data still mapped in writer
     uint64_t start_unused;     ///< Points to just after the last byte of actual data
-} gx_mmipc_head;
+} gx_mapc_head;
 
 
-//------------------------------------------------------------------------------
-/// Holds the state for an mmipc- including memory-mapped data pointers.
-typedef gx_mmipc {
+/// Holds the state for an mapc- including memory-mapped data pointers.
+
+typedef gx_mapc {
     struct gx_mfd     *_next, *_prev; ///< For resource pooling
     int                type;          ///< Reader or writer
     int                is_volatile;   ///< Used to record whether or not the file is RAM-based
 
     union {
         void          *head_map;
-        gx_mmipc_head *head;
+        gx_mapc_head *head;
     };
     void              *map;
 
 
 
-} gx_mmipc;
+} gx_mapc;
 
-gx_pool_init(gx_mmipc);
-
-
+gx_pool_init(gx_mapc);
 
 
 
 
-static GX_OPTIONAL gx_mmipc_open_w(gx_mmipc *mm, const char *path, int persistent, ) {
+
+
+static GX_OPTIONAL gx_mapc_open_w(gx_mapc *mm, const char *path, int persistent, ) {
 
 }
 
