8a7e23f77b97703c3eca53864488e055fd9b69c5
Joseph Wecker <jwecker@justin.tv>
2012-09-21 13:32:31 -0700

    Quick test from earlier looking at how atomic stderr writes can get without explicit (expensive) locking

---
+++ tst/test_stdout_mingling.c
@@ -0,0 +1,37 @@
+#include "./gx.h"
+#include "./gx_token.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <string.h>
+
+gx_error_initialize(GX_ERROR);
+
+int main(int argc, char **argv) {
+    int stat;
+    fork(); fork(); fork(); fork();
+
+    //dup2(STDERR_FILENO, STDERR_FILENO);
+    int i,j;
+    char outstr[1024] = {0};
+    char *p = outstr;
+    pid_t pid = getpid();
+    for(i = 0; i < 100; i++) {
+        unsigned int r1;
+        p = outstr;
+        X (gx_dev_random(&r1, sizeof(r1), 0)) X_ERROR;
+        int num = (r1 % 8) + 1;
+        for(j=0; j < num; j++) p += sprintf(p, " -=%u=- ", pid);
+        p += sprintf(p, "\n");
+        for(j=0; j < num; j++) p += sprintf(p, " -=%u=- ", pid);
+        fprintf(stderr, "%s\n\n", outstr);
+        gx_sleep(0,r1 & 0xffff);
+        //p[0] = '\n';
+        //p[1] = '\0';
+        //write(STDERR_FILENO, outstr, strlen(outstr));
+        //writev(STDERR_FILENO, (struct iovec[]){{outstr,strlen(outstr)}}, 1);
+    }
+    wait(&stat);
+    return 0;
+}
+
