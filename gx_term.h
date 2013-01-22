7880e3a7978e40c1bee6f3d894a138a6d1d27ef9
Joseph Wecker <joseph.wecker@gmail.com>
2013-01-22 11:30:01 -0800

    A couple of new quick term-stderr writers

--- gx_term.h
+++ gx_term.h
@@ -6,6 +6,8 @@
 #define C_TTL  "\e[38;5;196m"
 #define C_GR1  "\e[38;5;248m"
 #define C_GR2  "\e[38;5;240m"
+#define WSTDEs(STRING)        _( write(STDERR_FILENO, (STRING), strlen(STRING)) ) _ignore()
+#define WSTDEb(BUFFER,SIZE)   _( write(STDERR_FILENO, (BUFFER), (SIZE)) ) _ignore()
 #define WSTDE(CONTENT)        _( write(STDERR_FILENO, (CONTENT), sizeof(CONTENT)) ) _ignore()
 #define WSTDEx(CONTENT,TIMES) ({ int wstde_j; int wstde_mx=(TIMES); for(wstde_j=0; wstde_j<wstde_mx; wstde_j++) WSTDE(CONTENT); })
 
